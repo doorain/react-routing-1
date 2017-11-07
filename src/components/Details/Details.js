402f5b770cb2bd4d63b39270856f2dbe7bb29d85
Dorian Morgan <doorain@gmail.com>
2017-11-07 13:03:14 -0700

    mini project done

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { addToCart } from "../../ducks/reducer";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 import './Details.css';
 
@@ -9,19 +10,21 @@ function Details( { swag, history, addToCart } ) {
   function addAndRedirect() {
     addToCart( swag.id );
     history.push('/');
-  } 
+  }
 
   return (
-    <div id="Details__container">
+  <div id="Details__container">
+    <Link to="/" id="Details__storeLink">
       <p> { '<' } Back to store </p>
-      <img id="Details__img" src={ swag.img } alt="Product" width="400px" />
-      <p id="Details__title"> { swag.title } </p>
-      <p id="Details__price"> ${ swag.price } </p>
-      <button id="Details__addToCart" onClick={ addAndRedirect }> Add to Cart </button>
-      <p> { swag.description } </p>
-      <p> { swag.promo } </p>
-    </div>
-  )
+    </Link>
+    <img id="Details__img" src={ swag.img } alt="Product" width="400px" />
+    <p id="Details__title"> { swag.title } </p>
+    <p id="Details__price"> ${ swag.price } </p>
+    <button id="Details__addToCart" onClick={ addAndRedirect }> Add to Cart </button>
+    <p> { swag.description } </p>
+    <p> { swag.promo } </p>
+  </div>
+)
 }
 
 function mapStateToProps( state, ownProps ) {
